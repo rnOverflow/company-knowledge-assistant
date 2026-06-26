b5f9f899ff6ed34f49c556b4933e9bfb809a59e1
rnOverflow <aaryanwastaken17@gmail.com>
2026-06-26 23:33:05 +0530

    Optimize memory usage for Render

--- backend/app/rag/retriever.py
+++ backend/app/rag/retriever.py
@@ -3,20 +3,25 @@ from langchain_community.vectorstores import Chroma
 
 CHROMA_PATH = "backend/app/database/chroma_db"
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
 
-vectorstore = Chroma(
-    persist_directory=CHROMA_PATH,
-    embedding_function=embeddings
-)
+def get_vectorstore():
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    return Chroma(
+        persist_directory=CHROMA_PATH,
+        embedding_function=embeddings
+    )
+
 
 def retrieve_documents(
     query: str,
     k: int = 10,
     document_name: str = None
 ):
+
+    vectorstore = get_vectorstore()
 
     if document_name:
         docs = vectorstore.similarity_search(
